91a2d2d0496e2517f7a6f365db401b9a8e807841
G284872022 <2284872022@st.takushoku-u.ac.jp>
2024-05-30 15:25:45 +0900

    x

--- weather.js
+++ weather.js
@@ -1,49 +1,4 @@
-/*let data = {
-  "coord": {
-    "lon": 116.3972,
-    "lat": 39.9075
-  },
-  "weather": [
-    {
-      "id": 803,
-      "main": "Clouds",
-      "description": "曇りがち",
-      "icon": "04d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 9.94,
-    "feels_like": 8.65,
-    "temp_min": 9.94,
-    "temp_max": 9.94,
-    "pressure": 1022,
-    "humidity": 14,
-    "sea_level": 1022,
-    "grnd_level": 1016
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 2.65,
-    "deg": 197,
-    "gust": 4.84
-  },
-  "clouds": {
-    "all": 53
-  },
-  "dt": 1646542386,
-  "sys": {
-    "type": 1,
-    "id": 9609,
-    "country": "CN",
-    "sunrise": 1646520066,
-    "sunset": 1646561447
-  },
-  "timezone": 28800,
-  "id": 1816670,
-  "name": "北京市",
-  "cod": 200
-};*/
+
 
 ////////// 課題3-2 ここからプログラムを書こう
 /*console.log("緯度：" + data["coord"]["lon"]);
@@ -130,8 +85,9 @@ function showResult(resp) {
 		data = JSON.parse(data);
 	}
   console.log('通信成功');
-  
+
   let div1 = document.querySelector('div#result');
+  div1.remove();
   let pw = document.createElement('p');
   pw.textContent = "都市名：" + data["name"];
   div1.insertAdjacentElement('beforeend', pw);
